870563330e51ffb72ea95f6c520d0aba8ebd47b7
Md Tareq Miah <mdtareqmia25@gmail.com>
2026-07-09 15:32:25 +0600

    Task 3.3: Add User Education Relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,4 +38,9 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Role::class);
     }
+
+    public function educations(): HasMany
+    {
+        return $this->hasMany(Education::class);
+    }
 }
